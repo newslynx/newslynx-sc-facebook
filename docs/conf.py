47884bf39989aced3797c4dc8101a9e8cba044e4
abelsonlive <brianabelson@gmail.com>
2015-08-22 22:06:24 -0400

    test build

--- docs/conf.py
+++ docs/conf.py
@@ -204,10 +204,10 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    ('index', 'newslynx-sc-facebook.tex', u'newslynx-sc-facebook Documentation',
-     u'Brian Abelson', 'manual'),
-]
+# latex_documents = [
+#     ('index', 'newslynx-sc-facebook.tex', u'newslynx-sc-facebook Documentation',
+#      u'Brian Abelson', 'manual'),
+# ]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
@@ -234,10 +234,10 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    ('index', 'newslynx-sc-facebook', u'newslynx-sc-facebook Documentation',
-     [u'Brian Abelson'], 1)
-]
+# man_pages = [
+#     ('index', 'newslynx-sc-facebook', u'newslynx-sc-facebook Documentation',
+#      [u'Brian Abelson'], 1)
+# ]
 
 # If true, show URL addresses after external links.
 #man_show_urls = False
@@ -248,11 +248,11 @@ man_pages = [
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
-texinfo_documents = [
-    ('index', 'newslynx-sc-facebook', u'newslynx-sc-facebook Documentation',
-     u'Brian Abelson', 'newslynx-sc-facebook', 'This module contains all Sous Chefs which deal with Facebook via \`facepy\`.',
-     'Miscellaneous'),
-]
+# texinfo_documents = [
+#     ('index', 'newslynx-sc-facebook', u'newslynx-sc-facebook Documentation',
+#      u'Brian Abelson', 'newslynx-sc-facebook', 'This module contains all Sous Chefs which deal with Facebook via \`facepy\`.',
+#      'Miscellaneous'),
+# ]
 
 # Documents to append as an appendix to all manuals.
 #texinfo_appendices = []
